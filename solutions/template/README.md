d58b8d9c1523120c8fcd640cd19a38cc73393443
Akbar Odilov <akbar@odilov.de>
2021-07-31 12:11:21 +0200

    template for competetive programming

---
+++ solutions/template/README.md
@@ -0,0 +1,10 @@
+# Template
+This template presents some of the features of the C++ that
+are useful in competitive programming
+
+
+allows you to include the entire standard library ( iostream, vector, algorithm ... )
+
+```C++
+    #include <bits/stdc++.h>
+```
---
+++ solutions/template/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+#define in freopen( "in.txt", "r", stdin )
+#define out freopen( "out.txt", "w", stdout )
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define FOR( i, l, r ) for( int i = l; i < r; ++i )
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+
+int main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    // your solution comes here
+    return 0;
+}
