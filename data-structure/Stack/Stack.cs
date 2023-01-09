0525cf20bb6ada174afe43d7b9b9a819c75eb35b
odilov <akbar@odilov.de>
2023-01-09 18:08:39 +0100

    Add Simple Stack

---
+++ data-structure/Stack/Stack.cs
@@ -0,0 +1,11 @@
+﻿namespace Stack;
+
+public class Stack<T>
+{
+    private readonly T[] _item;
+    private int _index = -1;
+    public Stack() => _item = new T[10];
+    public int Count => _index + 1;
+    public void Push(T p0) => _item[++_index] = p0;
+    public T Pop() => _item[_index--];
+}
